1f285986489a6c68095d3fa44c66b8f18b616c14
201118063 <201118063@samsun.edu.tr>
2021-02-08 11:14:19 +0300

    Add project files.

---
+++ PersonelMVCUI/Controllers/PersonelController.cs
@@ -0,0 +1,30 @@
+﻿using PersonelMVCUI.Models.EntityFramework;
+using PersonelMVCUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonelMVCUI.Controllers
+{
+    public class PersonelController : Controller
+    {
+        PersonelDbEntities db = new PersonelDbEntities();
+        // GET: Personel
+        public ActionResult Index()
+        {
+            var model = db.Personel.ToList();
+            return View(model);
+        }
+
+        public ActionResult Yeni()
+        {
+            var model = new PersonelFormViewModels()
+            {
+                Departmans=db.Departman.ToList()
+            };
+            return View("PersonelForm",model);
+        }
+    }
+}
---
+++ PersonelMVCUI/ViewModels/PersonelFormViewModels.cs
@@ -0,0 +1,14 @@
+﻿using PersonelMVCUI.Models.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PersonelMVCUI.ViewModels
+{
+    public class PersonelFormViewModels
+    {
+        public IEnumerable<Departman> Departmans { get; set; }
+        public Personel Personels { get; set; }
+    }
+}
